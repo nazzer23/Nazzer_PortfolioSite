08ab0fbe0efca0de6ccd6c70897a683e1bd31242
Ben Vernazza <nazzer23@hotmail.co.uk>
2024-04-04 00:47:45 +0100

    Implemented Lazy Loading on slow rendering properties.

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,11 +1,12 @@
 <script type="ts">
-
-import GitHubProjects from "./GitHubProjects.svelte";
+  const githubProjectPromise = import("./GitHubProjects.svelte");
 </script>
 
 <section id="projects">
-    <div class="flex flex-col gap-6 justify-center items-center w-full">
-        <h1>my projects</h1>
-        <GitHubProjects/>
-    </div>
+  <div class="flex flex-col gap-6 justify-center items-center w-full">
+    <h1>my projects</h1>
+    {#await githubProjectPromise then { default: GitHubProjects }}
+      <GitHubProjects />
+    {/await}
+  </div>
 </section>
